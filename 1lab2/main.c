609f82e7dc313b7823e2c57d7fd9f8c679a0a635
Daniil <89664655+ucrdaniel@users.noreply.github.com>
2023-02-22 12:03:41 +0300

    Add files via upload

---
+++ 1lab2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+int c=5, d;
+double a=5, b;
+ scanf("%lf %d, &b, &d");
+ a+=b-2;
+printf("",a,b,e,d)
+ d+=++c+a;
+  printf
+a*=c--;
+ printf(a=%lf, b=%lf. c=%d,d=%d\n)
+d*=((c/=2)+--b+(a/=10));
+ printf
+
+}
